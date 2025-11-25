8505bb5e4580f6866733df3e1ad92d9899d5e551
RahKri <92612641+RahKri@users.noreply.github.com>
2025-11-25 14:35:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Configuration: YOUR NUMBER
-const WHATSAPP_NUMBER = "91 9999999999"; 
+const WHATSAPP_NUMBER = "91 9496793866"; 
 
 function contactWhatsApp(customMessage) {
     let msgToSend = customMessage;
